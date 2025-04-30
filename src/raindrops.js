c2f6df5ae2fb111cf2f90c00a307e50a9cab193b
David Kim <khk980703@hotmail.com>
2025-04-30 14:12:11 +1200

    add drop animation

--- src/raindrops.js
+++ src/raindrops.js
@@ -1,8 +1,10 @@
 export default function raindrops() {
   const bodyElement = document.querySelector('body')
+  const divElement = document.createElement('div')
   const raindropImg = document.createElement('img')
   raindropImg.src = './img/raindrops.png'
   raindropImg.className = 'raindrop'
-  bodyElement.appendChild(raindropImg)
+  bodyElement.appendChild(divElement)
+  divElement.appendChild(raindropImg)
   console.log(bodyElement)
 }
